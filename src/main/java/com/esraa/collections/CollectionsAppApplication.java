fe5c8c6ad766644e72aadf5607d239a88f624268
Esraa <Esraa@192.168.1.19>
2025-11-22 00:39:35 +0200

    set example

--- src/main/java/com/esraa/collections/CollectionsAppApplication.java
+++ src/main/java/com/esraa/collections/CollectionsAppApplication.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -20,22 +21,68 @@ public class CollectionsAppApplication {
 		
 		
 		Collection<Integer> c1 = new ArrayList<Integer>();
-		
-		Collection<Integer> c2 = new HashSet<Integer>();
-		
-		c1 = c2 ;
+//		
+//		Collection<Integer> c2 = new HashSet<Integer>();
+//		
+//		c1 = c2 ;
 		
 		List<Integer> l1 = (List)c1 ;
 		
 	//	Collection<Integer> c3 = new HashMap<Integer,Integer>() ;
 		
-		Set<Integer> nums = new HashSet<Integer>();
+		Set<Integer> nums = new HashSet<Integer>(); // cannot access set elements using index (unlike list)
 		nums.add(2);
 		nums.add(10);
 		nums.add(90);
 		nums.add(100);
 		
 		System.out.println(nums);
+		
+		
+		
+		// how we can access data inside set 
+		
+		      // 1- iterate over set elements
+		
+		          // a_ using for each
+		          System.out.println("using foreach"); 
+		     
+		         for (Integer i : nums)
+		         {System.out.println(i);}
+		
+		         
+		         
+		         
+		          // b_ using  ierattor 
+		         System.out.println("using  ierattor");
+		        Iterator<Integer> iterator = nums.iterator();
+		        while (iterator.hasNext())
+		        {
+		        	System.out.println(iterator.next());
+		        }
+		          
+		     
+		      //2_ check element existence  
+		        
+		        System.out.println(nums.contains(10));
+		        
+		        
+		      //3_ convert to list or array
+		        
+		          // a_ convert to Array 
+		        Integer[] numsArr = nums.toArray(new Integer[0]) ;
+		        
+		        
+		        
+		       
+		         //b_ convert to list 
+		        List<Integer> numsList = new ArrayList<>(nums);
+		        System.out.println(numsList.get(0));
+		        
+		        
+		         
+		         
+		         
 		
 		
 	}
